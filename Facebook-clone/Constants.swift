0cf08234b67e6720b9925bb7f7820b30b6243aba
YoannLTH <yoann.lathuiliere@gmail.com>
2017-10-04 23:16:15 +0300

    style(config) : add constant file

---
+++ Facebook-clone/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Facebook-clone
+//
+//  Created by yoann lathuiliere on 04/10/2017.
+//  Copyright © 2017 yoann lathuiliere. All rights reserved.
+//
+
+import Foundation
+
+struct Key {
+  static let KEY_UID = "uid"
+  static let SIGNIN_TO_FEED_SEGUE_NAME = "goToFeed"
+  static let FEED_TO_SIGNIN_SEGUE_NAME = "goToSignIn"
+}
